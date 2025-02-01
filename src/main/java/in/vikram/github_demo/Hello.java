344e12cb9464e0b5d97173ad1276f1ba0953a84d
Vikram Kumar Sharma <vks8210980102@gmail.com>
2025-02-01 15:59:29 +0530

    Added the project to git hub repo

---
+++ src/main/java/in/vikram/github_demo/Hello.java
@@ -0,0 +1,9 @@
+package in.vikram.github_demo;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hi This is Vikram Sharma");
+    }
+
+}
---
+++ src/test/java/in/vikram/github_demo/GithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vikram.github_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
